de2dc447cc910a2a29b98db871c87eff1af40099
Alexander Laudino <93599746+alexander-laudino@users.noreply.github.com>
2022-07-14 05:54:12 -0400

    Add _createBoardArray method in GameBoard; extract inner methods from drawBoard in displayController

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -1,9 +1,14 @@
 const GameBoard = (() => {
-  const _board = [];
-  const _row = ["X", "X", "X"];
-  _board[0] = Object.assign([], _row);
-  _board[1] = Object.assign([], _row);
-  _board[2] = Object.assign([], _row);
+  const _board = _createBoardArray();
+
+  function _createBoardArray() {
+    const _board = [];
+    const _row = ["", "", ""];
+    _board[0] = Object.assign([], _row);
+    _board[1] = Object.assign([], _row);
+    _board[2] = Object.assign([], _row);
+    return _board;
+  }
 
   function getBoard() {
     return _board;
@@ -34,34 +39,17 @@ const Player = (marker) => {
   return { getMarker };
 };
 
-const Game = (() => {
-  const _playerOne = Player("X");
-  const _playerTwo = Player("O");
-
-  function getPlayers() {
-    return [_playerOne, _playerTwo];
-  }
-
-  return { getPlayers: getPlayers };
-})();
-
 const displayController = (() => {
   function drawBoard() {
-    let board = document.createElement("div");
-    board.setAttribute("class", "board");
     let gameBoard = document.querySelector(".gameBoard");
+    let board = _createBoardDiv();
     let rowIndex = 1;
     for (let row of GameBoard.getBoard()) {
-      let rowDiv = document.createElement("div");
-      rowDiv.setAttribute("class", "row");
-      rowDiv.setAttribute("id", `row${rowIndex}`);
+      let rowDiv = _createRowDiv(rowIndex);
       let columnIndex = 1;
       for (let column of row) {
-        let columnDiv = document.createElement("div");
-        columnDiv.setAttribute("class", `column${columnIndex}`);
-        columnDiv.setAttribute("id", `row${rowIndex}-column${columnIndex}`);
-        let markerText = document.createElement("p");
-        markerText.textContent = column;
+        let columnDiv = _createColumnDiv(columnIndex, rowIndex);
+        let markerText = _createMarkerPara(column);
         columnDiv.appendChild(markerText);
         rowDiv.appendChild(columnDiv);
         columnIndex++;
@@ -72,10 +60,49 @@ const displayController = (() => {
     gameBoard.appendChild(board);
   }
 
+  function _createBoardDiv() {
+    let board = document.createElement("div");
+    board.setAttribute("class", "board");
+    return board;
+  }
+
+  function _createRowDiv(rowIndex) {
+    let rowDiv = document.createElement("div");
+    rowDiv.setAttribute("class", `row${rowIndex}`);
+    return rowDiv;
+  }
+
+  function _createColumnDiv(columnIndex, rowIndex) {
+    let columnDiv = document.createElement("div");
+    columnDiv.setAttribute("class", `column${columnIndex}`);
+    columnDiv.setAttribute("data-row", `${rowIndex}`);
+    columnDiv.setAttribute("data-column", `${columnIndex}`);
+    return columnDiv;
+  }
+
+  function _createMarkerPara(column) {
+    let markerText = document.createElement("p");
+    markerText.textContent = column;
+    return markerText;
+  }
+
   function deleteCurrentBoard() {
     let board = document.querySelector(".board");
     board.parentNode.removeChild(board);
   }
 
   return { drawBoard: drawBoard, deleteCurrentBoard: deleteCurrentBoard };
+})();
+
+const Game = (() => {
+  const _playerOne = Player("X");
+  const _playerTwo = Player("O");
+
+  function getPlayers() {
+    return [_playerOne, _playerTwo];
+  }
+
+  function playRound() {}
+
+  return { getPlayers: getPlayers };
 })();
